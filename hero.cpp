ad6b4206a2240d4df96ea566f02c46adc38200d1
AliHosseinPour85 <ali380h641p@gmail.com>
2025-07-04 17:50:43 +0330

    Implement methods defeatMonster and pickUpItem of Hero

--- hero.cpp
+++ hero.cpp
@@ -86,3 +86,184 @@ std::vector<Perk> Hero::GetPerk()
 {
     return perks;
 }
+
+void Hero::defeatMonster(Monster* monster) {
+    if (monster->getName() == MonsterName::DRACULA)
+    {
+        try
+        {
+            std::vector <Item> sendItem;
+            int a = 1;
+            for (size_t i = 0; i < items.size(); i++)
+            {
+                std::cout << a << ". " << items[i].getItemName() << std::endl;
+                a++;
+            }
+            for (size_t i = 0; i < items.size(); i++)
+            {
+                std::cout << "Enter the Item you want to use (100 for end):" ;
+                int entekhab;
+                std::cin >> entekhab;
+                if (entekhab == 100)
+                {
+                    break;
+                }
+                sendItem.push_back(items[entekhab - 1]);
+            }
+            
+            monster->receiveDefeat(location , sendItem);
+            actionsLeft--;
+            for (size_t i = 0; i < sendItem.size(); i++)
+            {
+                for (size_t j = 0; j < items.size(); j++)
+                {
+                    if (items[j].getItemName() == sendItem[i].getItemName())
+                    {
+                        items.erase(items.begin() + j);
+                    }
+                }  
+            }
+            
+        }
+        catch(std::invalid_argument &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        catch(std::logic_error &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        
+    }
+    else{
+        try
+        {
+            std::vector <Item> sendItem;
+            int a = 1;
+            for (size_t i = 0; i < items.size(); i++)
+            {
+                std::cout << a << ". " << items[i].getItemName() << std::endl;
+                a++;
+            }
+            for (size_t i = 0; i < items.size(); i++)
+            {
+                std::cout << "Enter the Item you want to use (100 for end):" ;
+                int entekhab;
+                std::cin >> entekhab;
+                if (entekhab == 100)
+                {
+                    break;
+                }
+                sendItem.push_back(items[entekhab - 1]);
+            }
+            
+            monster->receiveDefeat(location , sendItem);
+            actionsLeft--;
+            for (size_t i = 0; i < sendItem.size(); i++)
+            {
+                for (size_t j = 0; j < items.size(); j++)
+                {
+                    if (items[j].getItemName() == sendItem[i].getItemName())
+                    {
+                        items.erase(items.begin() + j);
+                    }
+                }  
+            }
+            
+        }
+        catch(std::invalid_argument &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        catch(std::logic_error &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        
+    }
+}
+
+void Hero::pickUpItem(const std::array<Item* , 35>& AllItems)
+{ 
+    if (name == HeroName::ARCHAEOLOGIST)
+    {
+        std::vector<Location> near = Map::getNeighboringPlaces(location);
+        std::vector<Item*> Wanted;
+        near.push_back(location);
+        std::cout << "there is the items you can pick :" << std::endl;
+        int a = 1;
+        for (size_t i = 0; i < AllItems.size(); i++)
+        {
+            for (size_t j = 0; j < near.size(); j++)
+            {
+                
+                if (AllItems[i]->getItemStatus()==ItemStatus::ON_BOARD && AllItems[i]->getLocation() == near[j])
+                {
+                    std::cout << a << ". " << AllItems[i]->getItemName() << std::endl;
+                    Wanted.push_back(AllItems[i]);
+                    a++;
+                }
+            }
+        }
+        int itemWant;
+        while (true)
+        {
+            std::cout << "Enter what Items you want to pick (100 for end):";
+            std::cin >> itemWant;
+            if (itemWant == 100)
+            {
+                break;
+            }
+            if (Wanted[itemWant - 1]->getItemStatus() == ItemStatus::IN_INVENTORY)
+            {
+                continue;
+            }
+            else{
+                Wanted[itemWant - 1]->setItemStatus(ItemStatus::IN_INVENTORY);
+                items.push_back(*Wanted[itemWant - 1]);
+                
+            }
+            
+        }
+        
+    }
+    
+    if (name == HeroName::MAYOR)
+    {
+        std::vector<Item*> Wanted;
+
+        int a = 1;
+        for (size_t i = 0; i < AllItems.size(); i++)
+        {
+            
+            if (AllItems[i]->getItemStatus()==ItemStatus::ON_BOARD && AllItems[i]->getLocation() == location)
+            {
+                std::cout << a << ". " << AllItems[i]->getItemName() << std::endl;
+                Wanted.push_back(AllItems[i]);
+                a++;
+            }
+        }
+        int itemWant;
+        while (true)
+        {
+            std::cout << "Enter what Items you want to pick (100 for end):";
+            std::cin >> itemWant;
+            if (itemWant == 100)
+            {
+                break;
+            }
+            if (Wanted[itemWant - 1]->getItemStatus() == ItemStatus::IN_INVENTORY)
+            {
+                continue;
+            }
+            else{
+                Wanted[itemWant - 1]->setItemStatus(ItemStatus::IN_INVENTORY);
+                items.push_back(*Wanted[itemWant - 1]);
+                
+            }
+            
+        }
+    }
+    
+    actionsLeft--;
+}
