ea60204934d2fd3889dec6d301a9398a4f8a6960
moh104 <mg1game1m@gmail.com>
2025-07-04 12:28:12 +0330

    Implement remaining Monster methods

--- monster.cpp
+++ monster.cpp
@@ -49,3 +49,119 @@ void Monster::setRequiredItemStrength(int strength)
     }
     requiredItemStrength = strength;
 }
+
+void Monster::movement (int moveCount , Location target) noexcept
+{
+    int distance = Map::getDistance(location , target);
+
+    if(moveCount >= distance)
+    {
+        location = target;
+        return;
+    }
+
+    for (int i = 0 ; i < 19 ; ++i)
+    {
+        Location otherLocation = static_cast<Location>(i);
+        distance = Map::getDistance(location , otherLocation);
+        int remainingDistance = Map::getDistance(otherLocation , target);
+        if (distance == moveCount && remainingDistance == (Map::getDistance(location , target) - moveCount))
+        {
+            location = otherLocation;
+        }
+    }
+}
+
+bool Monster::attack(Hero* hero , Villager* villager) const noexcept
+{
+    if(hero && hero->receiveAttack())
+    {
+        Map::increaseHorror();
+        return true;
+    }
+    else if (villager)
+    {
+        Map::increaseHorror();
+        villager->receiveAttack();
+        return true;
+    }
+    return false;
+}
+
+bool Monster::isActive() const noexcept
+{
+    return active;
+}
+
+void Monster::kill() noexcept
+{
+    active = false;
+}
+
+const unordered_map<Location , bool>& Monster::getMatStatus() const noexcept
+{
+    return matStatus;
+}
+
+void Monster::receiveDefeat(Location heroLocation , const std::vector<Item>& items)
+{
+    if (!isActive())
+    {
+        throw std::logic_error("Dracula has already been killed");
+    }
+    for (auto const& part : matStatus)
+    {
+        if (!part.second)
+        {
+            throw std::logic_error("The monster's mat mission is not complete");
+        }
+    }
+    if (heroLocation != location)
+    {
+        throw std::invalid_argument("To kill monster, the hero must be in that monster's position");
+    }
+    int totalStrength = 0;
+    for (auto const& item : items)
+    {
+        if (item.getItemType() != requiredItemType)
+        {
+            throw std::invalid_argument("Invalid item type used to kill the monster");
+        }
+        totalStrength += item.getStrength();
+    }
+    if (totalStrength < requiredItemStrength)
+    {
+        throw std::logic_error("The total power of the items is not enough to kill monster");
+    }
+
+    kill();
+}
+
+unsigned int Monster::numOfPartsSolved() const noexcept
+{
+    unsigned int partsSolved = 0;
+    for (auto const& part : matStatus)
+    {
+        if (part.second)
+        {
+            ++partsSolved;
+        }
+    }
+
+    return partsSolved;
+}
+
+
+void Monster::doOperationOnMat(Location location)
+{
+    auto it = matStatus.find(location);
+    if (it == matStatus.end())
+    {
+        throw std::invalid_argument("Invalid location for monster's mat");
+    }
+    if (it->second)
+    {
+        throw std::logic_error("The operation of this location on the monster's mat has already been done");
+    }
+    it->second = true;
+}
